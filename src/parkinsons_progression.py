9a9885c34a6d8586cbeeca05aab602fe7dfead71
unknown <ranminervi@yahoo.com>
2023-03-19 21:58:30 +0200

    add comments

--- src/parkinsons_progression.py
+++ src/parkinsons_progression.py
@@ -18,9 +18,17 @@ loadInstance = LoadAndPreprocess(protein_train_path, peptide_train_path, train_c
 # ~160 to ~210 peptides types taken per visit 
 #lstm that get timebased visit peptides e.g : 
 #                V1  V2  V3  V4
-#  visit month    0   3    6   12
-#  Pep1           21   45  42  97
-#  Pep2           2    32  44  111 
-#  Pep3           15   51  511  511
-#  PRotein i Val 21   545  65  90
-# targets are udprs  
+#  visit month    0   6    12  24   36  48  60  108
+#  Pep1           21   45  42  97               .
+#  Pep2           2    32  44  111              .
+#  Pep3           15   51  511  511             .
+#  .
+#  .
+#  .
+#  Protein i Val  21   545  65  90
+# udprs_1          3    5   (8)   9
+# udprs_2          12  14  (13)   15            
+# udprs_3          22  34  (45)   34
+# udprs_4          22  45  (56)   45
+# targets are udprs[i] - for fit #1  udprs month #6  for fit2 udprs for M12  
+#predict values in parentisis (val) for monthes 6 12 24 
