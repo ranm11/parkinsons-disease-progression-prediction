6beed4355f1a9b3654a81117828433c6d2ae8e1b
Ran Minervy <rminervy@synamedia.com>
2023-03-29 17:54:17 +0300

    design

--- src/parkinsons_progression.py
+++ src/parkinsons_progression.py
@@ -10,7 +10,7 @@ loadInstance = LoadAndPreprocess(protein_train_path, peptide_train_path, train_c
 
 # prediction model design 
 
-#lstm network recieve as input 
+
 # patients_data array which compose of
 # fit(patient_data) -> [Patient_ID, Visit_month, peptide_type,peptideAbundance ]
 #input_data(visit_month (0-~36),)
@@ -21,14 +21,53 @@ loadInstance = LoadAndPreprocess(protein_train_path, peptide_train_path, train_c
 #  visit month    0   6    12  24   36  48  60  108
 #  Pep1           21   45  42  97               .
 #  Pep2           2    32  44  111              .
-#  Pep3           15   51  511  511             .
+#  Pep3           15   51  511 511              .
 #  .
 #  .
 #  .
 #  Protein i Val  21   545  65  90
-# udprs_1          3    5   (8)   9
-# udprs_2          12  14  (13)   15            
-# udprs_3          22  34  (45)   34
-# udprs_4          22  45  (56)   45
+# udprs_1          3    5   8   9
+# udprs_2          12  14   13   15            
+# udprs_3          22  34   45   34
+# udprs_4          22  45   56   45
 # targets are udprs[i] - for fit #1  udprs month #6  for fit2 udprs for M12  
-#predict values in parentisis (val) for monthes 6 12 24 
+#as first step 
+# we got solid data for months 0,6,12,24,36,48,60,108
+# train on data up to M48 predict udprs for M60 
+
+#for step 2  - layer concatenation
+#  udprs history
+#  peptide history
+# concatenate into decision
+#
+#from tensorflow.keras.layers import Input, Dense, concatenate
+#from tensorflow.keras.models import Model
+#
+## Define the input layers
+#input1 = Input(shape=(100,))
+#input2 = Input(shape=(50,))
+#
+## Define the first hidden layer for input 1
+#hidden1 = Dense(64, activation='relu')(input1)
+#
+## Define the second hidden layer for input 2
+#hidden2 = Dense(32, activation='relu')(input2)
+## Define the first GRU layer for input 1
+#gru1 = GRU(32)(input1)
+#
+## Define the second GRU layer for input 2
+#gru2 = GRU(16)(input2)
+## Combine the two inputs and their hidden layers
+#concatenated = concatenate([hidden1, hidden2 ,gru1,gru2])
+#
+## Define the output layer
+#output = Dense(1, activation='sigmoid')(concatenated)
+#
+## Define the model
+#model = Model(inputs=[input1, input2], outputs=output)
+#
+## Compile the model
+#model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+#
+## Fit the model to the data
+#model.fit([X1, X2], y, epochs=10, batch_size=32)
